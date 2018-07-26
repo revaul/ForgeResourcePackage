85d4b20524cb8e72e9558daf1b38052dc92adf65
Ryan Evaul <ryan.evaul@snhu.edu>
2018-07-26 11:18:05 -0400

    Armor Enchantments

--- src/main/java/com/idtech/item/ItemCustomArmor.java
+++ src/main/java/com/idtech/item/ItemCustomArmor.java
@@ -1,13 +1,16 @@
 package com.idtech.item;
 
-import net.minecraft.entity.Entity;
-
 import com.idtech.BaseMod;
 
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemArmor;
 import net.minecraft.item.ItemStack;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.world.World;
 
 public class ItemCustomArmor extends ItemArmor {
 
@@ -27,5 +30,31 @@ public class ItemCustomArmor extends ItemArmor {
 			return "examplemod:textures/models/armor/custom_armor_layer_2.png";
 		}
 		return "examplemod:textures/models/armor/custom_armor_layer_1.png";
+	}
+//This override it not part of the curriculum but I thought it was cool...
+	@Override
+	public void onArmorTick(World world, EntityPlayer player, ItemStack itemStack) {
+		// TODO Auto-generated method stub
+		super.onArmorTick(world, player, itemStack);
+		player.heal(10);
+		
+		//ID Numbers -> https://minecraft.gamepedia.com/Status_effect
+		
+		player.addPotionEffect(new PotionEffect(Potion.getPotionById(1),100,2));
+		
+		player.addPotionEffect(new PotionEffect(Potion.getPotionById(3),5,1));
+		/**
+		player.addPotionEffect(new PotionEffect(Potion.getPotionById(5),100,1));
+		player.addPotionEffect(new PotionEffect(Potion.getPotionById(8),100,1));
+		player.addPotionEffect(new PotionEffect(Potion.getPotionById(12),100,1));
+		player.addPotionEffect(new PotionEffect(Potion.getPotionById(13),100,1));
+		player.addPotionEffect(new PotionEffect(Potion.getPotionById(14),100,1));
+		player.addPotionEffect(new PotionEffect(Potion.getPotionById(16),100,1));
+		player.addPotionEffect(new PotionEffect(Potion.getPotionById(23),5,1));
+		player.addPotionEffect(new PotionEffect(Potion.getPotionById(25),100,1));
+		player.addPotionEffect(new PotionEffect(Potion.getPotionById(26),100,1));
+		player.addPotionEffect(new PotionEffect(Potion.getPotionById(28),100,1));
+		player.addPotionEffect(new PotionEffect(Potion.getPotionById(30),100,1));
+		**/
 	}
 }
